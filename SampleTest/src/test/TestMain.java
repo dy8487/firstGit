9b7f5718bdb93495eb6f6a56abd954488484938e
dykwak <phnewma@hotmail.com>
2013-04-22 22:15:24 +0900

    springSecurity

---
+++ SampleTest/src/test/TestMain.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dy.kwak
+ * Date: 13. 4. 21
+ * Time: 오후 11:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("1234");
+    }
+
+}
